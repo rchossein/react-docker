7db3554309ba32fcdba130220bb55f789192f88a
Hossein Kalbasi <hossein.kalbasi@reactivecore.com>
2019-03-28 17:07:34 -0400

    initial commit

---
+++ build/precache-manifest.19d04220441b25b6f04cc301317e0687.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "441284d33a8fe56187ae",
+    "url": "/static/js/main.079eea04.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "441284d33a8fe56187ae",
+    "url": "/static/css/main.6b400084.chunk.css"
+  },
+  {
+    "revision": "d3c1b82975112aafea9fecacde2fcd48",
+    "url": "/index.html"
+  }
+];
